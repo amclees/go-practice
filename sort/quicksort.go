c29563814e59860612bce46103024f6f5170170b
Andrew McLees <andrewmclees5@gmail.com>
2018-09-14 23:38:42 -0700

    Add quicksort

---
+++ sort/quicksort.go
@@ -0,0 +1,39 @@
+package sort
+
+func Quicksort(s []int64) {
+  if len(s) < 2 {
+    return
+  }
+
+  p := partition(s)
+  Quicksort(s[:p + 1])
+  Quicksort(s[p + 1:])
+}
+
+func partition(s []int64) int {
+  pivot := findPivot(s)
+  i := 0
+  j := len(s) - 1
+  for {
+    for ; s[i] < pivot; i++ {
+    }
+
+    for ; s[j] > pivot; j-- {
+    }
+
+    if i >= j {
+      return j
+    }
+
+    t := s[i]
+    s[i] = s[j]
+    s[j] = t
+
+    i += 1
+    j -= 1
+  }
+}
+
+func findPivot(s []int64) int64 {
+  return s[0]
+}
