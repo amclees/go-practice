1a9ed5f3ce6048c6b92ca172b5d374e8b5d275f6
Andrew McLees <andrewmclees5@gmail.com>
2018-09-14 20:09:18 -0700

    Start with insertion sort

---
+++ sort/insertion_sort_test.go
@@ -0,0 +1,19 @@
+package sort
+
+import "testing"
+
+func TestInsertionSort(t *testing.T) {
+  a := []int64{6, 2, 5, 2, 4, 3}
+  b := []int64{2, 2, 3, 4, 5, 6}
+  InsertionSort(a)
+  fail := false
+  for i, _ := range a {
+    if a[i] != b[i] {
+      fail = true
+      break
+    }
+  }
+  if fail {
+    t.Errorf("Expected a to be %v, got %v", b, a)
+  }
+}
