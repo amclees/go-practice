b504f026da82b6a7b1750bbcd6a47abc7bb1f896
Andrew McLees <andrewmclees5@gmail.com>
2018-09-16 11:39:49 -0700

    Add priority queue

---
+++ queue/priority_queue_test.go
@@ -0,0 +1,20 @@
+package queue
+
+import "testing"
+
+func TestPriorityQueue(t *testing.T) {
+  q := PriorityQueue{}
+  q.Init(10)
+
+  q.Enqueue(5, 10)
+  q.Enqueue(6, 5)
+  q.Enqueue(3, 4)
+
+  expected := []int64{5, 10, 4}
+  for i := range expected {
+    d := q.Dequeue()
+    if d != expected[i] {
+      t.Errorf("Expected dequeue to return %d, was %d", expected[i], d)
+    }
+  }
+}
