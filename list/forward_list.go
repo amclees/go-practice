839958e9afe5f4ca01f683b0ed6ec48161f19d84
Andrew McLees <andrewmclees5@gmail.com>
2018-09-15 21:46:07 -0700

    Add singly linked list

---
+++ list/forward_list.go
@@ -0,0 +1,76 @@
+package list
+
+type ForwardList struct {
+  first *Node
+  length int
+}
+
+type Node struct {
+  next *Node
+  val int64
+}
+
+func (list *ForwardList) last() *Node {
+  current := list.first
+  for ; current.next != nil; current = current.next {}
+  return current
+}
+
+func (list *ForwardList) Append(value int64) {
+  list.length += 1
+  node := Node{val: value}
+  if list.first == nil {
+    list.first = &node
+    return
+  }
+  list.last().next = &node
+}
+
+func (list *ForwardList) Prepend(value int64) {
+  list.length += 1
+  node := Node{val: value, next: list.first}
+  list.first = &node
+}
+
+func (list *ForwardList) IndexOf(value int64) int {
+  i := 0
+  for current := list.first; current != nil; current = current.next {
+    if current.val == value {
+      return i
+    }
+    i += 1
+  }
+  return -1
+}
+
+func (list *ForwardList) Get(index int) (int64, bool) {
+  i := 0
+  for current := list.first; current != nil; current = current.next {
+    if i == index {
+      return current.val, true
+    }
+    i += 1
+  }
+  return 0, false
+}
+
+func (list *ForwardList) Remove(index int) bool {
+  if list.first == nil {
+    return false
+  }
+  i := 0
+  current := list.first
+  for ; i < index - 1; current = current.next {
+    if current.next == nil {
+      return false
+    }
+    i += 1
+  }
+  current.next = current.next.next
+  list.length -= 1
+  return true
+}
+
+func (list *ForwardList) Length() int {
+  return list.length
+}
---
+++ list/forward_list_test.go
@@ -0,0 +1,48 @@
+package list
+
+import "testing"
+
+func TestForwardList(t *testing.T) {
+  list := ForwardList{}
+
+  list.Append(5)
+  // 5
+
+  if list.Length() != 1 {
+    t.Errorf("Expected list length %d, was %d", 1, list.Length())
+  }
+
+  for i := 0; i < 5; i++ {
+    list.Append(int64(i))
+  }
+  // 5 0 1 2 3 4
+
+  list.Prepend(1)
+  // 1 5 0 1 2 3 4
+
+  first, _ := list.Get(0)
+  if first != 1 {
+    t.Errorf("Expected first value of list after prepend to be 1, was %d", first)
+  }
+
+  removed := list.Remove(1)
+  // 1 0 1 2 3 4
+  if !removed {
+    t.Errorf("Expected removal to succeed")
+  }
+
+  second, _ := list.Get(1)
+  if second != 0 {
+    t.Errorf("Expected second value of list after deletion to be 0, was %d", second)
+  }
+
+  index := list.IndexOf(2)
+  if index != 3 {
+    t.Errorf("Expected IndexOf(2) to be 3, was %d", index)
+  }
+
+  index = list.IndexOf(5)
+  if index != -1 {
+    t.Errorf("Expected IndexOf(5) to be -1, was %d", index)
+  }
+}
