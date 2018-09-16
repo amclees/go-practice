abdccec3f2d82e93b4779941be49ac682fbf45bc
Andrew McLees <andrewmclees5@gmail.com>
2018-09-16 11:20:18 -0700

    Add queue

---
+++ queue/queue.go
@@ -0,0 +1,38 @@
+package queue
+
+type Queue struct {
+  next *Node
+  end *Node
+  size int
+}
+
+type Node struct {
+  val int64
+  next *Node
+}
+
+func (q *Queue) Enqueue(d int64) {
+  q.size += 1
+
+  n := Node{val: d}
+
+  if q.size == 1 {
+    q.next = &n
+  } else {
+    q.end.next = &n
+  }
+
+  q.end = &n
+}
+
+func (q *Queue) Dequeue() (int64, bool) {
+  if q.size == 0 {
+    return 0, false
+  } else if q.size == 1 {
+    q.end = nil
+  }
+  q.size -= 1
+  d := q.next.val
+  q.next = q.next.next
+  return d, true
+}
---
+++ queue/queue_test.go
@@ -0,0 +1,24 @@
+package queue
+
+import "testing"
+
+func TestQueue(t *testing.T) {
+  q := Queue{}
+
+  _, success := q.Dequeue()
+  if success {
+    t.Errorf("Expected Dequeue to fail on empty queue")
+  }
+
+  q.Enqueue(1)
+  q.Enqueue(2)
+  q.Enqueue(3)
+
+  expected := []int64{1, 2, 3}
+  for i := range expected {
+    found, _ := q.Dequeue()
+    if found != expected[i] {
+      t.Errorf("Expected dequeue to return %d, got %d", expected[i], found)
+    }
+  }
+}
