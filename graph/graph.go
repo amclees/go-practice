bb15926c363e599ee2b9f86405a5896cb1b5d8e7
Andrew McLees <andrewmclees5@gmail.com>
2018-09-18 07:34:21 -0700

    Revise Graph interface for weights

--- graph/graph.go
+++ graph/graph.go
@@ -2,15 +2,16 @@ package graph
 
 type Graph interface {
 	AddNode(id int64, val interface{})
-	AddEdge(src, dst int64) bool
+	AddEdge(src, dst, w int64)
 
-	Node(id int64) interface{}
-	HasEdge(src, dst int64) bool
+	Node(id int64) (bool, interface{})
+	Edge(src, dst int64) (bool, int64)
 
 	RemoveNode(id int64) bool
 	RemoveEdge(src, dst int64) bool
 
 	Nodes() []int64
-	Edges(id int64) []int64
-	Size() int64
+	Edges(id int64) [][2]int64
+	EdgeCount() int
+	NodeCount() int
 }
